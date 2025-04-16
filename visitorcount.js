dc9364ff7ffbddda52447a70de5593345f15c0e8
nodysey <officiallacka2de@gmail.com>
2025-04-16 17:25:00 -0400

    lol update

---
+++ visitorcount.js
@@ -0,0 +1,19 @@
+async function viewCount() {
+    const wrapper = document.querySelector(".flex-row.grow-row.visitorcount");
+    // get and increment visitor count
+    var count = await fetch("https://hnds.lewolfyt.cc/api/visitor-count")
+    .then((res) => {return res.text()});
+    // manipulate view count and turn it into an array
+    count = count.padStart(8,"-");
+    wrapper.innerHTML = "";
+    for (let idx = 0; idx < count.length; idx++) {
+        const itm = count.charAt(idx);
+        if (itm === "-") {
+            wrapper.innerHTML += `<span>0</span>`;
+        } else {
+            wrapper.innerHTML += `<span>${itm}</span>`;
+        }
+    };
+};
+
+viewCount();
