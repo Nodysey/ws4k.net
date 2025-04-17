ac991b9e58ceebcee3c1b91df9b02ed5240ff60e
nodysey <officiallacka2de@gmail.com>
2025-04-17 08:16:59 -0400

    the

--- visitorcount.js
+++ visitorcount.js
@@ -1,15 +1,25 @@
 async function viewCount() {
     const wrapper = document.querySelector(".flex-row.grow-row.visitorcount");
     // get and increment visitor count
-    var count = await fetch("https://hnds.lewolfyt.cc/api/visitor-count")
-    .then((res) => {return res.text()});
+    try {
+        var count = await fetch("https://hnds.lewolfyt.cc/api/visitor-count")
+        .then((res) => {return res.text()});
+    } catch (err) {
+        console.error(err);
+        wrapper.innerHTML = "";
+        const str = " error! "
+        for (let idx = 0; idx < str.length; idx++) {
+            const itm = str.charAt(idx);
+            wrapper.innerHTML += `<span>${itm}</span>`;
+        };    
+    }
     // manipulate view count and turn it into an array
     count = count.padStart(8,"-");
     wrapper.innerHTML = "";
     for (let idx = 0; idx < count.length; idx++) {
         const itm = count.charAt(idx);
         if (itm === "-") {
-            wrapper.innerHTML += `<span>0</span>`;
+            wrapper.innerHTML += `<span>-</span>`;
         } else {
             wrapper.innerHTML += `<span>${itm}</span>`;
         }
